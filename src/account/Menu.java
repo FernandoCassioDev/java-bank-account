c9891fe4b1fcd7f6ced3abe991cf7ac6183d0acd
Fernando Cassio <fernandocassiodev@gmail.com>
2024-09-14 18:54:27 -0300

    feat: cria as classes Cores, Menu e Account

---
+++ src/account/Menu.java
@@ -0,0 +1,94 @@
+package account;
+
+import java.util.Scanner;
+
+import account.model.Account;
+import account.util.Cores;
+
+public class Menu {
+  public static void main(String[] args) {
+
+    Account c1 = new Account(1, 123, 1, "joe hippie", 100000);
+
+    c1.visualizar();
+    c1.sacar(50000);
+    c1.visualizar();
+    c1.depositar(50000);
+    c1.visualizar();
+
+    try (Scanner read = new Scanner(System.in)) {
+
+      int option;
+
+      while (true) {
+        System.out
+            .println(Cores.TEXT_YELLOW_BRIGHT + Cores.ANSI_BLACK_BACKGROUND + "\n\n***************************************");
+        System.out.println("                                       ");
+        System.out.println("        BANCO DO BRAZIL COM Z          ");
+        System.out.println("                                       ");
+        System.out.println("***************************************");
+        System.out.println("                                       ");
+        System.out.println("         1 - Criar Conta               ");
+        System.out.println("         2 - Listar todas as Contas    ");
+        System.out.println("         3 - Buscar Conta por Numero   ");
+        System.out.println("         4 - Atualizar dados da Conta  ");
+        System.out.println("         5 - Apagar Conta              ");
+        System.out.println("         6 - Sacar                     ");
+        System.out.println("         7 - Depositar                 ");
+        System.out.println("         8 - Transferir valores entre Contas    ");
+        System.out.println("         9 - Sair                      ");
+        System.out.println("                                       ");
+        System.out.println("***************************************");
+        System.out.println("Entre com a opção desejada:                      ");
+        System.out.println("                                       ");
+
+        option = read.nextInt();
+
+        if (option == 9) {
+          System.out.println(Cores.TEXT_WHITE_BOLD + "\nBanco do Brazil com Z - O seu Futuro começa aqui!");
+          read.close();
+          about();
+          System.exit(0);
+        }
+
+        switch (option) {
+          case 1:
+            System.out.println(Cores.TEXT_WHITE_BOLD + "Criar Conta\n\n");
+            break;
+          case 2:
+            System.out.println(Cores.TEXT_WHITE_BOLD +"Listar todas as Contas\n\n");
+            break;
+          case 3:
+            System.out.println(Cores.TEXT_WHITE_BOLD +"Consultar dados da Conta - por número\n\n");
+            break;
+          case 4:
+            System.out.println(Cores.TEXT_WHITE_BOLD +"Atualizar dados da Conta\n\n");
+            break;
+          case 5:
+            System.out.println(Cores.TEXT_WHITE_BOLD +"Apagar a Conta\n\n");
+            break;
+          case 6:
+            System.out.println(Cores.TEXT_WHITE_BOLD +"Saque\n\n");
+            break;
+          case 7:
+            System.out.println(Cores.TEXT_WHITE_BOLD +"Depósito\n\n");
+            break;
+          case 8:
+            System.out.println(Cores.TEXT_WHITE_BOLD +"Tranferência entre Contas\n\n");
+            break;
+          default:
+            System.out.println(Cores.TEXT_RED_BOLD +"\nOpção Inválida\n");
+            break;
+        }
+      }
+    }
+  }
+
+  public static void about() {
+    System.out.println("\n***************************************");
+    System.out.println("Projeto Desenvolvido por: Fernando Cassio");
+    System.out.println("fernandocassiodev@gmail.com");
+    System.out.println("github.com/fernandocassiodev");
+    System.out.println("\n***************************************");
+  }
+}
---
+++ src/account/model/Account.java
@@ -0,0 +1,93 @@
+package account.model;
+
+public class Account {
+  private int numero;
+  private int agencia;
+  private int tipo;
+  private String titular;
+  private float saldo;
+
+  public Account(int numero, int agencia, int tipo, String titular, float saldo) {
+    this.numero = numero;
+    this.agencia = agencia;
+    this.tipo = tipo;
+    this.titular = titular;
+    this.saldo = saldo;
+  }
+
+  public int getNumero() {
+    return numero;
+  }
+
+  public void setNumero(int numero) {
+    this.numero = numero;
+  }
+
+  public int getAgencia() {
+    return agencia;
+  }
+
+  public void setAgencia(int agencia) {
+    this.agencia = agencia;
+  }
+
+  public int getTipo() {
+    return tipo;
+  }
+
+  public void setTipo(int tipo) {
+    this.tipo = tipo;
+  }
+
+  public String getTitular() {
+    return titular;
+  }
+
+  public void setTitular(String titular) {
+    this.titular = titular;
+  }
+
+  public float getSaldo() {
+    return saldo;
+  }
+
+  public void setSaldo(float saldo) {
+    this.saldo = saldo;
+  }
+
+  public boolean sacar(float value) {
+    if (this.getSaldo() < value) {
+      System.out.println("\nSaldo Insuficiente!");
+      return false;
+    }
+
+    this.setSaldo(this.getSaldo() - value);
+    return true;
+  }
+
+  public void depositar(float value) {
+    this.setSaldo(this.getSaldo() + value);
+  }
+
+  public void visualizar(){
+    String type = "";
+
+    switch (this.tipo) {
+      case 1:
+        type = "Conta Corrente";
+        break;
+      case 2:
+        type = "Conta Poupança";
+        break;
+    }
+
+    System.out.println("\n\n************************************");
+    System.out.println("Dados da Conta:");
+    System.out.println("\n\n************************************");
+    System.out.println("Numero da Conta: " + this.numero);
+    System.out.println("Agência: " + this.agencia);
+    System.out.println("Tipo da conta: " + type);
+    System.out.println("Titular: " + this.titular);
+    System.out.println("Saldo: " + this.saldo);
+  }
+}
